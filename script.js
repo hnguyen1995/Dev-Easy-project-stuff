6e5b252e6785811d99aa8a60d0e400e42716d1a4
hnguyen1995 <hnguyen1@una.edu>
2025-03-18 11:04:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ function myFunction() {
 
 
 let facts = [
-    "Reed: He has two brothers.", "Richie: His favorite color is purple.", "Hayden: He likes to swim.", "Jaxon: He broke his finger.", "Victor: He is ambidextrous.", "Olivia: She can say the alphabet backwards.", "Corey: He is not famous.", "Tatum: She was born in Louisiana.", "Kaden: He has never had a peanut butter jelly sandwich.", "Dylan: He is allergic to cats.", "Samuel: He likes trains.", "Daniel: He knows the identity of beastcarp1.", "Matthew: He has a dog.", "Jacob: He hates UNA parking.", "Landon: He has 3 dogs.", "Carly: She was adopted from China.", "Cohen: He used to have an orange hummer H3.", "Carson: He plays on the rugby team.", "Gage: He said UNA basketball sold the bag.", "Corbin: He said the Great Wall of China is 13,000 miles long.", "Hai: He hates JavaScript.", "Laura: She loves the bible verse Romans 5:8!", "Erika: She loves running.", "Jackson: 🦭🦭", "Griffin: He has eaten a pound of ground beef everyday for the last 6 months.", "Arun: He said that there is not much parking space at UNA.", "Connor: He enjoys kickboxing."
+    "Reed: He has two brothers.", "Richie: His favorite color is purple.", "Hayden: He likes to swim.", "Jaxon: He broke his finger.", "Victor: He is ambidextrous.", "Olivia: She can say the alphabet backwards.", "Corey: He is not famous.", 
+  "Tatum: She was born in Louisiana.", "Kaden: He has never had a peanut butter jelly sandwich.", "Dylan: He is allergic to cats.", "Samuel: He likes trains.", "Daniel: He knows the identity of beastcarp1.", "Matthew: He has a dog.", "Jacob: He hates UNA parking.",
+  "Landon: He has 3 dogs.", "Carly: She was adopted from China.", "Cohen: He used to have an orange hummer H3.", "Carson: He plays on the rugby team.", "Gage: He said UNA basketball sold the bag.", "Corbin: He said the Great Wall of China is 13,000 miles long.", "Hai: He hates JavaScript.",
+  "Laura: She loves the bible verse Romans 5:8!", "Erika: She loves running.", "Jackson: 🦭🦭", "Griffin: He has eaten a pound of ground beef everyday for the last 6 months.", "Arun: He said that there is not much parking space at UNA.", "Connor: He enjoys kickboxing.",
+  "David: Didn't provide a fact."
 ];
 
 function displayFacts() {
